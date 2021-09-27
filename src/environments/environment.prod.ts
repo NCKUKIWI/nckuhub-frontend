cf614530e6244855694fe8c18cb3e19bee05d987
nick0916 <nickliao2013@gmail.com>
2021-09-27 20:26:59 +0800

    fix: bug about universal

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production : false,
+  title : 'NCKUHUB DEV',
+  appEndPoint : 'http://localhost:8080',
+  logoutUrl: '/login',
+  resetParamFlag: true,
+  errorRedirectFlag: false
 };
