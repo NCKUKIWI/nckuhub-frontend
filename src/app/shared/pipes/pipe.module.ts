aef01fa3499b8444079e80236792f5b87cfc9c0b
nick0916 <nickliao2013@gmail.com>
2021-10-03 16:12:48 +0800

    feat: add pipe 'nextLine'

--- src/app/shared/pipes/pipe.module.ts
+++ src/app/shared/pipes/pipe.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { NextLinePipe } from './next-line.pipe';
 
 // for shared pipe module to sharedModule
 
 @NgModule({
     declarations: [
+        NextLinePipe
     ],
     exports: [
+        NextLinePipe
     ]
 })
 export class PipeModule {}
