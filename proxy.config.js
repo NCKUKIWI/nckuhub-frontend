51970d48565afe6b880394eeef8eb683c1cfe424
nick0916 <nickliao2013@gmail.com>
2021-11-25 19:38:55 +0800

    refactor: setting proxy

--- proxy.config.js
+++ proxy.config.js
@@ -5,17 +5,6 @@ const PROXY_CONFIG = {
         "logLevel": "debug",
         "changeOrigin": true,
         "pathRewrite": {"^/api": ""},
-        "onProxyRes": function(pr, req, res) {
-            console.log("pr header", pr.headers);
-            console.log("req", req.session);
-            console.log("res", res.session);
-            if (pr.headers['set-cookie']) {
-                const cookies = pr.headers['set-cookie'].map(cookie =>
-                    cookie.replace(/;(\ )*secure/gi, '')
-                );
-                pr.headers['set-cookie'] = cookies;
-            }
-        },
     }
 }
 module.exports = PROXY_CONFIG;
