0ca2ac02aa2c59c19588772f8de473f7cd8a054b
wyt890301 <73593132+wyt890301@users.noreply.github.com>
2021-12-28 20:41:18 +0800

    feat: add wishlist function

--- src/app/pages/course/components/course-content/course-content.component.ts
+++ src/app/pages/course/components/course-content/course-content.component.ts
@@ -11,25 +11,79 @@ import { CourseModel } from '../../models/Course.model';
     styleUrls: ['./course-content.component.scss'],
 })
 export class CourseContentComponent implements OnInit {
-    // 紀錄該課程的留言資料
-    private score_data: CourseWithCommentModel;
-    // 用於score_data中的courseInfo
-    private course_data: CourseModel;
-    // 用於score_data中的comment
-    private comment_data: CourseComment[];
+    // 該課程的留言資料(包含評分、課程資料、留言)
+    private scoreData: CourseWithCommentModel;
+    // score_data中的courseInfo(課程資料)
+    private courseData: CourseModel;
+    // score_data中的comment(留言)
+    private commentData: CourseComment[];
+    // 使用者的wishlist
+    private wishList: number[] = [];
 
     constructor(private router: ActivatedRoute, private courseService: CourseService) {
+        // 抓取該課程的資料
         this.router.params.subscribe((param) => {
             const courseId: number = param.course_id;
 
-            this.courseService.fetchCourseWithComments(courseId).subscribe((commentData) => {
-                this.score_data = commentData;
-                this.course_data = commentData.courseInfo;
-                this.comment_data = commentData.comment;
-                console.log(this.score_data, this.course_data);
+            // 將抓到資料分成scoreData, courseData, commentData
+            this.courseService.fetchCourseWithComments(courseId).subscribe((courseCommentData) => {
+                this.scoreData = courseCommentData;
+                this.courseData = courseCommentData.courseInfo;
+                this.commentData = courseCommentData.comment;
+
+                // 課程評分四捨五入到整數
+                this.scoreData.got = Math.round(parseFloat(this.scoreData.got)).toString();
+                this.scoreData.sweet = Math.round(parseFloat(this.scoreData.sweet)).toString();
+                this.scoreData.cold = Math.round(parseFloat(this.scoreData.cold)).toString();
             });
         });
     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        // 取得 願望清單
+        this.getUserWishList();
+    }
+
+    /**
+     * 取得 願望清單
+     */
+    getUserWishList(): void {
+        this.wishList = JSON.parse(localStorage.getItem('wishList'));
+        if (this.wishList == null) {
+            this.wishList = [];
+        }
+    }
+
+    /**
+     * 開啟 該課程的大綱
+     * @private
+     */
+    private openOutline(deptCode, courseCode): void {
+        let url = 'http://course-query.acad.ncku.edu.tw/crm/course_map/course.php?dept=' + deptCode + '&cono=' + courseCode;
+        window.open(url, '_blank');
+    }
+
+    /**
+     * 新增&刪除 願望清單
+     */
+    setWishlist(id: number): void {
+        const chooseCourseId = id;
+        if (this.wishList.includes(id)) {
+            // 刪除 該課程
+            const index = this.wishList.findIndex((x) => x == id);
+            this.wishList.splice(index, 1);
+            localStorage.setItem('wishList', JSON.stringify(this.wishList));
+        } else {
+            //新增 該課程
+            this.wishList.push(id);
+            localStorage.setItem('wishList', JSON.stringify(this.wishList));
+        }
+
+        // if (userData.now_wishlist.includes(chooseCourseId)){
+        //   wishlistRemove(chooseCourseId);
+        // }
+        // else{
+        //   wishlistAdd(chooseCourseId);
+        // }
+    }
 }
