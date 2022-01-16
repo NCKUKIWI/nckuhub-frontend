ccf377848eb743e5fa818448a777ddd20f1c9229
ecfack <ecfack007@gmail.com>
2022-01-16 20:34:43 +0800

    refactor: convert course.sass to course.scss

--- src/app/pages/course/components/wish-list/wish-list.component.ts
+++ src/app/pages/course/components/wish-list/wish-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseModel } from '../../models/Course.model';
 
 @Component({
     selector: 'app-wish-list',
@@ -6,7 +7,42 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./wish-list.component.scss'],
 })
 export class WishListComponent implements OnInit {
-    constructor() {}
+    wishList: CourseModel[] = [];
+    constructor() { }
 
-    ngOnInit(): void {}
+    ngOnInit(): void { }
+
+    deleteWish(wishId:Number):void {
+        this.wishList=this.wishList.filter((course) => course.id !== wishId);
+    }
+
+    /**
+     * 回傳 傳入課程 的所屬系所簡稱
+     * @param deptID 傳入課程的系所代號
+     * @param deptName 傳入課程的系所名稱
+     * @returns 傳入課程的所屬系所簡稱
+     */
+     deptTransCat(deptID: string, deptName: string): string {
+        let category: string;
+        switch (deptID) {
+            case 'A9':
+                category = '通';
+                break;
+            case 'A6':
+                category = '服';
+                break;
+            case 'A7':
+                category = '國';
+                break;
+            case 'A1':
+                category = '外';
+                break;
+            case 'A2':
+                category = '體';
+                break;
+            default:
+                category = deptName.substring(0, 1);
+        }
+        return category;
+    }
 }
--- src/app/pages/course/services/wish-list.service.ts
+++ src/app/pages/course/services/wish-list.service.ts
@@ -16,4 +16,12 @@ export class WishListService {
 
     constructor(private userService: UserService) {
     }
+
+    //wishList：感興趣的課程，用於加入課表，查看心得
+    //TODO：建立Subject用來發送每次更動後的wishlist，addWish，removeWish
+    //用法：componemt訂閱Subject，只使用addWish，removeWish
+    //checkVali：檢查wishList長度，是否有對應到課程，清除重複元素
+    //addWish：增加wish在wishList，再讓subject next wishList
+    //removeWish：減少wish在wishList，再讓subject next wishList
+    //uploadWis：回傳wishList到DB，定量回傳？定時回傳？
 }
