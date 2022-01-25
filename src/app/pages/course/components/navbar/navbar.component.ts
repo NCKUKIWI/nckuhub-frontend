571b31d4d7985e22225bb942a19eecc42df13408
winniepopu <winniepopu@gmail.com>
2022-01-25 22:34:36 +0800

    finish switch pages in navbar

--- src/app/pages/course/components/navbar/navbar.component.ts
+++ src/app/pages/course/components/navbar/navbar.component.ts
@@ -1,12 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
+import { CourseModel } from '../../models/Course.model';
+import { CourseService } from '../../services/course.service';
+import { take, filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
+    styleUrls: ['./navbar.component.scss', '../../course.component.css'],
 })
-export class NavbarComponent implements OnInit {
-    constructor() {}
 
-    ngOnInit(): void {}
+export class NavbarComponent implements OnInit, OnChanges, DoCheck {
+    public href: string = "";
+    private windowTab: String = "";
+    private isDropdownOpen: Boolean=false;
+    private allCourseInNewSemester: CourseModel[] = [];
+
+
+    constructor(
+        private router: Router,
+        private courseService: CourseService
+    ) {}
+
+    ngOnInit(): void {
+        this.href = this.router.url;
+        this.updateWindowTab();   
+        this.getCourseData();
+    }
+    private getCourseData(): void {
+        console.log("123");
+        // 加filter是因為可能會收到空陣列
+        this.courseService.getCourseData().pipe(filter(data => data.length != 0), take(1)).subscribe(
+            (courseData) => {
+                this.allCourseInNewSemester = courseData;
+                console.log("願望清單獲取總課程成功", this.allCourseInNewSemester.length);
+            },
+            (err: any) => {
+                if (err) {
+                    console.error(err);
+                }
+            }
+        );
+    }
+    ngOnChanges():void{
+
+    }
+
+    ngDoCheck(): void {
+        this.href = this.router.url;
+        if (this.windowTab != this.href.split('/')[2])
+            this.updateWindowTab();
+    }
+
+    updateWindowTab(){
+        this.windowTab = this.href.split('/')[2];
+    }
+    
+
+
 }
