b0ea94bd4c70d26be13d7bbd7df0d031af12623b
winniepopu <winniepopu@gmail.com>
2022-02-08 22:20:45 +0800

    show search-filer courses on navbar

--- src/app/pages/course/course.module.ts
+++ src/app/pages/course/course.module.ts
@@ -13,9 +13,10 @@ import { WriteCommentComponent } from './components/write-comment/write-comment.
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DynamicDialogModule } from 'primeng/dynamicdialog';
 import { DialogService } from 'primeng/dynamicdialog';
+import { SearchFilterPipe } from './search-filter.pipe';
 
 @NgModule({
-    declarations: [CourseContentComponent, CourseComponent, TimetableComponent, NavbarComponent, CourseSearchComponent, WishListComponent, CourseUserComponent, WriteCommentComponent],
+    declarations: [CourseContentComponent, CourseComponent, TimetableComponent, NavbarComponent, CourseSearchComponent, WishListComponent, CourseUserComponent, WriteCommentComponent, SearchFilterPipe],
     imports: [CourseRoutingModule, CommonModule, SharedModule, FormsModule, ReactiveFormsModule, DynamicDialogModule],
     providers: [DialogService],
 })
