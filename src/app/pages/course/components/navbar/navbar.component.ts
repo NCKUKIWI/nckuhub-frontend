9db795d02ad8a3315a17922242546c0d7c881ba1
winniepopu <winniepopu@gmail.com>
2022-02-25 21:33:29 +0800

    open the inner page of search result

--- src/app/pages/course/components/navbar/navbar.component.ts
+++ src/app/pages/course/components/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnChanges, OnInit, DoCheck } from '@angular/core';
+import { Component, OnChanges, OnInit, Optional } from '@angular/core';
 import { Router } from '@angular/router';
 import { CourseModel } from '../../models/Course.model';
 import { CourseService } from '../../services/course.service';
 import { take, filter } from 'rxjs/operators';
-  
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { CourseContentComponent } from '../course-content/course-content.component';
+
+
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
@@ -11,7 +14,6 @@ import { take, filter } from 'rxjs/operators';
 })
 
 export class NavbarComponent implements OnInit, OnChanges {
-    // public href: string = "";
     public query: string = "";
     public isDropdownOpen: Boolean=false;
     private allCourseInNewSemester: CourseModel[] = [];
@@ -22,26 +24,27 @@ export class NavbarComponent implements OnInit, OnChanges {
 
     constructor(
         private router: Router,
-        private courseService: CourseService
+        private courseService: CourseService,
+        @Optional()
+        public ref: DynamicDialogRef,
+        private dialogService: DialogService,
+
     ) {}
 
     ngOnInit(): void {
-        // this.href = this.router.url;
-        this.getCourseData();
+        this.getAllCourseData();
         
     }
 
     /**
      *  打API拿 完整課程資料，取出部分初始化 展示用課程資料 和 有評論的課程資料
      */
-     getCourseData(): void {
+     getAllCourseData(): void {
         // 加filter是因為可能會收到空陣列
-        this.courseService.getCourseData().pipe(filter(data => data.length !== 0), take(1)).subscribe(
+        this.courseService.getAllCourseData().pipe(filter(data => data.length !== 0), take(1)).subscribe(
             (courseData) => {
                 this.allCourseInNewSemester = courseData;
                 this.displayCourseList = this.allCourseInNewSemester.slice(0, this.MAX_COURSE_DISPLAY_NUM);
-                // this.allCourseListWithComment = this.allCourseInNewSemester.filter((course) => course.commentNum > 0);
-                // console.log('get course data', courseData.length);
             },
             (err: any) => {
                 if (err) {
@@ -55,14 +58,28 @@ export class NavbarComponent implements OnInit, OnChanges {
         return this.courseService.deptTransCat(deptID,deptName)
    }
 
+    /**
+     * 為課程 打開課程內頁
+     * @param courseId 要打開課程內頁的課程的id
+     */
+    openCoursePage(courseId: number): void {
+        this.ref = this.dialogService.open(CourseContentComponent, {
+            width: '100%',
+            height: '100%',
+            baseZIndex: 10000,
+            transitionOptions: null,
+            style: { marginTop: '-75px' },
+            data: { courseId },
+        });
+
+        this.ref.onClose.subscribe(() => {
+            console.log('The dialog was closed');
+            this.router.navigateByUrl('/');
+        });
+    }
+
     ngOnChanges():void{
         console.log(this.query)
     }
-    // UpdateSearchCourseList():void{
-    //     // this.searchCourseList = this.displayCourseList;
-    //     console.log("this.result.length:",this.result.length);
-    //     console.log(this.result)
-    // }
-
 
 }
