ce7477399038205dc0daa8b50e942524355b751f
winniepopu <winniepopu@gmail.com>
2022-03-20 01:01:27 +0800

    delete consolelog of searchPipe

--- src/app/shared/pipes/search-filter.pipe.ts
+++ src/app/shared/pipes/search-filter.pipe.ts
@@ -9,8 +9,6 @@ export class SearchFilterPipe implements PipeTransform {
   transform(value: any, args?: any): any {
       if(!value)return null;
       if(!args)return value;
-      console.log("args: ", args)
-
       args = args.toLowerCase();
 
       return value.filter(function(data){
